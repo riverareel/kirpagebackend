48b034b04472cbf105c41ef398acc5ccb6b9cc8f
riverareel <108993316+riverareel@users.noreply.github.com>
2022-07-30 01:09:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ import json
 from time import sleep
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+import random
 from telebot import TeleBot
 from fastapi.responses import RedirectResponse
-bot = TeleBot("5463120320:AAEDXKo6B3yzK2kZqDtX-j6M9Km4Sd54kgA")
+bot1 = TeleBot("5463120320:AAEDXKo6B3yzK2kZqDtX-j6M9Km4Sd54kgA")
+bot2 = TeleBot("5314173046:AAHm7n4vMDMhWK4x6NHYmsWOYskQrQsLcdk")
+bot3 = TeleBot("5426530816:AAHNb9br4nHhyqgDlUe4p3FvOXu6n8QWNiQ")
+bot4 = TeleBot("5475071987:AAFSrIi56f29kcUaB0z6R_OhNX0xEkwJOVI")
+bot5 = TeleBot("5522125769:AAGVhGmhFkeWbNlJawIDm576b1-yALcpRmE")
+bot6 = TeleBot("5571413724:AAHpbw84ptHTnGSGsXJMLAVDT7npctXB3BQ")
+bot7 = TeleBot("5533057599:AAH5K824MTTiRdhscV-Wb8Dk-MJYW1io058")
+
 import time
 
 app = FastAPI()
@@ -58,7 +66,8 @@ Link WhatsApp  :</pre> https://wa.me/62{userData.phoneNumber}"""
     messagecid = -1001574503712
     while True:
         try:
-            bot.send_message(messagecid, constructString, parse_mode="HTML", disable_web_page_preview=True)
+            sender = random.choice([bot1, bot2, bot3, bot4, bot5, bot6, bot7])
+            sender.send_message(messagecid, constructString, parse_mode="HTML", disable_web_page_preview=True)
             break
         except:
             sleep(3)
